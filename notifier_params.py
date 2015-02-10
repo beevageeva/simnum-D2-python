c3495edfd459a47e6f9aadb2ebeffe815d4e0cde
beatrice <beatrice@localhost.localdomain>
2015-02-10 22:51:41 +0000

    10.02.2015

--- notifier_params.py
+++ notifier_params.py
@@ -6,8 +6,8 @@ from constants import z0_0, zf_0, z0_1, zf_1
 
 #second element in array is whether to calculate maxpoint velocity: I don't know what happens when multiple points for the max
 #TODO max speed does not show expected value
-#plots = {"3d": False, "dim0": [0.5 * (zf_0 + z0_0), False], "dim1": [0.5 * (zf_1 + z0_1), False], "color": True}
-plots = {"3d": True, "dim0": [0.5 * (zf_0 + z0_0), False], "dim1": [0.5 * (zf_1 + z0_1), False], "color": True}
+plots = {"3d": False, "dim0": [0.5 * (zf_0 + z0_0), False], "dim1": [0.5 * (zf_1 + z0_1), False], "color": True}
+#plots = {"3d": True, "dim0": [0.5 * (zf_0 + z0_0), False], "dim1": [0.5 * (zf_1 + z0_1), False], "color": True}
 #plots = {"3d": False, "color": True}
 
 
