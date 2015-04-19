b40468a920e2b1a9e3f6f31aa2e4fc5c3ed3622e
Beatrice Popescu Braileanu <bpopescu@colibri.ll.iac.es>
2015-04-19 20:34:59 +0100

    bm

--- visual_plot_simple.py
+++ visual_plot_simple.py
@@ -1,8 +1,8 @@
 import matplotlib
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.ticker import FormatStrFormatter
-#useWindow = False
-useWindow = True
+useWindow = False
+#useWindow = True
 if useWindow:
 	matplotlib.use('TkAgg')
 else:
@@ -15,8 +15,8 @@ from scipy.fftpack import fft,fftfreq#forFourierTransform
 
 from notifier_params import plots, plotAnalitical
 
-#saveImages = True
-saveImages = False
+saveImages = True
+#saveImages = False
 
 
 
